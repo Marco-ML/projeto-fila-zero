afa171c6a11b63e6f9451f0588d25324d294240e
Marco <76064619+Marco-ML@users.noreply.github.com>
2025-05-10 18:44:30 -0300

    feat: teste da integração do banco de dados API

---
+++ teste_banco_de_dados.py
@@ -0,0 +1,13 @@
+from supabase import create_client, Client
+
+# Substitua pelos seus dados reais
+url = "https://wjelqoiakkyljdxupmyf.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqZWxxb2lha2t5bGpkeHVwbXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5MTIyMjQsImV4cCI6MjA2MjQ4ODIyNH0.tVkJdo_6Tu2blnfgSiBdaoYM5b2635nYuaR5MzRiRGo"
+supabase: Client = create_client(url, key)
+
+# Função para buscar dados da tabela
+def buscar_dados():
+    response = supabase.table("alunos").select("*").execute()
+    return response
+
+buscar_dados()
